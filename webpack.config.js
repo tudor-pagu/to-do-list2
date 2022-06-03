3afd7cb36ee60379b063ea2f32f03353543115bb
ze-jeff123 <104032457+ze-jeff123@users.noreply.github.com>
2022-06-03 14:36:34 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         clean: true,
         assetModuleFilename : '[name][ext]'
     },
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
 
     module: {
         rules: [
